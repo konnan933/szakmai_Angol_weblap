723595beef14f9b42cfa9ac035fc0dc8e5290969
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-04-14 20:10:59 +0200

    Kezdő kinézet

--- javascript.js
+++ javascript.js
@@ -1,43 +1,43 @@
 window.addEventListener("load", init);
 
-
 function ID(elem) {
-    return document.getElementById(elem);
+  return document.getElementById(elem);
 }
-function QS(elem){
-    return document.querySelector(elem);
+function QS(elem) {
+  return document.querySelector(elem);
 }
-function $(elem){
-    return document.querySelectorAll(elem);
+function $(elem) {
+  return document.querySelectorAll(elem);
 }
-function CLASS(elem){
-    return document.getElementsByClassName
+function CLASS(elem) {
+  return document.getElementsByClassName;
 }
 var i = 0;
-var kepek = ['kepek/kep_1.jpg','kepek/kep_2.jpg','kepek/kep_3.jpg','kepek/kep_4.jpg','kepek/kep_5.jpg'];
-var toltes = function() {
-    $("hatter").attr("style", "background-image:url('" + kepek[i] + "')");
-};
+var kepek = [
+  "kepek/kep_1.jpg",
+  "kepek/kep_2.jpg",
+  "kepek/kep_3.jpg",
+  "kepek/kep_4.jpg",
+  "kepek/kep_5.jpg",
+];
 
-function init(){
-    toltes;
-    kepBetoltes();
-
+function init() {
+  kepBetoltes();
 }
-function kepBetoltes(){
-    ID("hatter").style.backgroundImage = "url("+kepek[i]+")";
-    setInterval(function(){
-    ID("hatter").style.opacity="0"
-    ID("hatter").style.transitionDuration="1000ms"
-    ID("hatter").style.backgroundImage = "url("+kepek[i]+")";
-    ID("hatter").style.opacity="1"
-    ID("hatter").style.transitionDuration="1000ms"
+function kepBetoltes() {
+  ID("hatter").style.backgroundImage = "url(" + kepek[1] + ")";
+  setInterval(function () {
+    ID("hatter").style.opacity = "0";
+    ID("hatter").style.transitionDuration = "1000ms";
+    ID("hatter").style.backgroundImage = "url(" + kepek[i] + ")";
+    ID("hatter").style.opacity = "1";
+    ID("hatter").style.transitionDuration = "1000ms";
     //https://stackoverflow.com/questions/3489270/wait-until-images-in-background-css-are-loaded
-    console.log(i);
-    if (i < kepek.length - 1) {
-        i++;
+    if (i === 4) {
+      i = 0;
     } else {
-        i = 0;
+      i++;
     }
-    },3000)    
+    console.log(i);
+  }, 4000);
 }
